b11b3923061f440e0d9e7db3b428e88271eb7b59
Muhammad Bintang Al-Fath <alfathbintangmuhammad@gmail.com>
2025-04-18 05:00:10 +0700

    Membuat atribut pada kelas employee modular dengan membuat class untuk masing-masing atribut yang terkait, Mengubah beberapa primitive data type dengan tipe data objek, Mengubah parameter constrctor Employee menyesuaikan dengan tipe data baru,Menyederhanakan method setMonthlySalary dan getAnnualIncomeTax

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -1,105 +1,215 @@
 package lib;
 
 import java.time.LocalDate;
-import java.time.Month;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Employee {
 
+	// Membuat enum gender sebagai pengganti primitive tipe data boolean
+	enum Gender {
+		MALE, 
+		FEMALE,
+	}
+	
+	// Membuat enum grade untuk menampung nilai konstan salary
+	enum Grade {
+		grade1(3000000), 
+		grade2(5000000), 
+		grade3(7000000);
+		
+		private final int baseSalary;
+			Grade(int baseSalary){
+			this.baseSalary = baseSalary;
+		}
+			
+		public int getBaseSalary() {
+			return baseSalary;
+		}
+		
+	}
+	
+	// Membuat class Spouse untuk menampung atribut spouseName dan spouseIdNumber
+	class Spouse {
+		private String name;
+		private String idNumber;
+		
+		public Spouse(String name, String idNumber) {
+			super();
+			this.name = name;
+			this.idNumber = idNumber;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public String getIdNumber() {
+			return idNumber;
+		}
+		
+		public void setIdNumber(String idNumber) {
+			this.idNumber = idNumber;
+		}
+	}
+	
+	// Membuat class Child untuk menampung atribut childName dan childIdNumber
+	class Child {
+		private String name;
+		private String idNumber;
+		
+		public Child(String name, String idNumber) {
+			this.name = name;
+			this.idNumber = idNumber;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public String getIdNumber() {
+			return idNumber;
+		}
+		
+		
+		public void setIdNumber(String idNumber) {
+			this.idNumber = idNumber;
+		}
+	}
+	
+	// Membuat class Salary untuk menampung atribut monthlySalary, otherMonthlyIncome, dan annualDeductive
+	 class Salary {
+		
+		private int monthlySalary;
+		private int otherMonthlyIncome;
+		private int annualDeductible;
+		private Grade grade;
+		
+		
+		public Salary(Grade grade, int otherMonthlyIncome, int annualDeductible) {
+			super();
+			this.grade = grade;
+			this.otherMonthlyIncome = otherMonthlyIncome;
+			this.annualDeductible = annualDeductible;
+		}
+		
+		/**
+		 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
+		 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
+		 */
+		
+		// Membuat lebih ringkas method setMonthlySalary
+		public void setMonthlySalary(Grade grade) {	
+			int baseSalary = grade.getBaseSalary();
+	        salary.monthlySalary = isForeigner ? (int) (baseSalary * 1.5) : baseSalary;
+		}
+		
+		public int getMonthlySalary() {
+			return monthlySalary;
+		}
+		
+		public void setAnnualDeductible(int deductible) {	
+			salary.annualDeductible = deductible;
+		}
+		
+		public int getAnnualDeductible() {
+			return annualDeductible;
+		}
+		
+		// Memperbaiki nama setter untuk monthlyIncome
+		public void setOtherMonthlyIncome(int income) {
+			salary.otherMonthlyIncome = income;
+		}
+		
+		public int getOtherMonthlyIncome() {
+			return otherMonthlyIncome;
+		}
+		
+		
+	}
+	
 	private String employeeId;
 	private String firstName;
 	private String lastName;
 	private String idNumber;
 	private String address;
 	
-	private int yearJoined;
-	private int monthJoined;
-	private int dayJoined;
-	private int monthWorkingInYear;
+	// Menggunakan tipe data objek Spouse 
+	private Spouse spouse;
+	
+	// Menggunakan tipe data List<Objek> dari Child
+	private List<Child> children;
+	
+	// Menggabungkan 4 atribut year, month, day, dan monthWorking dalam satu atribut
+	// Menggabungkan dalam tipe data LocalDate
+	private LocalDate joinedDate;
 	
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
 	
-	private int monthlySalary;
-	private int otherMonthlyIncome;
-	private int annualDeductible;
+	// Mengubah tipe data primtive boolean dengan enum
+	private Gender gender; 
 	
-	private String spouseName;
-	private String spouseIdNumber;
-
-	private List<String> childNames;
-	private List<String> childIdNumbers;
+	// Menggunakan tipe data objek Salary
+	private Salary salary;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, LocalDate joinedDate, boolean isForeigner, Gender gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
 		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
+		this.joinedDate = joinedDate;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
-		
-		childNames = new LinkedList<String>();
-		childIdNumbers = new LinkedList<String>();
 	}
 	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
 	
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}
+	public void setSpouse(Spouse spouse) {
+		this.spouse = new Spouse(spouse.name, spouse.idNumber);
 	}
 	
-	public void setAnnualDeductible(int deductible) {	
-		this.annualDeductible = deductible;
-	}
-	
-	public void setAdditionalIncome(int income) {	
-		this.otherMonthlyIncome = income;
-	}
-	
-	public void setSpouse(String spouseName, String spouseIdNumber) {
-		this.spouseName = spouseName;
-		this.spouseIdNumber = idNumber;
+	public void setSalary(Grade grade, int otherMonthlyIncome, int annualDeductive) {
+		this.salary = new Salary(grade, otherMonthlyIncome, annualDeductive);
 	}
 	
 	public void addChild(String childName, String childIdNumber) {
-		childNames.add(childName);
-		childIdNumbers.add(childIdNumber);
-	}
+        this.children.add(new Child(childName, childIdNumber));
+    }
 	
 	public int getAnnualIncomeTax() {
-		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
-		LocalDate date = LocalDate.now();
-		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
-			monthWorkingInYear = 12;
-		}
-		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
-	}
+        int monthsWorked = calculateMonthsWorkedInCurrentYear();
+        
+        // Menyederhanakan parameter output dengan pembuatan variabel
+        boolean isSingle = spouse == null;
+        int numberOfChildren = children.size();
+
+        return TaxFunction.calculateTax(
+                salary.monthlySalary,
+                salary.otherMonthlyIncome,
+                monthsWorked,
+                salary.annualDeductible,
+                isSingle,
+                numberOfChildren
+        );
+    }
+	
+	// Memisahkan method untuk menghitung lama waktu bekerja dalam bulan
+	private int calculateMonthsWorkedInCurrentYear() {
+        LocalDate today = LocalDate.now();
+        if (joinedDate.getYear() == today.getYear()) {
+            return today.getMonthValue() - joinedDate.getMonthValue() + 1;
+        } else {
+            return 12;
+        }
+    }
+	
+	
 }
